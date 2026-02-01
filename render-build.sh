6d77fd4c8a6143ba0b1f976e8728499f43c441aa
Ameer Hamza <2022cs17@student.uet.edu.pk>
2026-02-01 17:55:12 +0500

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Install Microsoft ODBC Driver 17 for SQL Server
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
